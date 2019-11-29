6237d18f334298993d56708f2dacd44b81fa5f58
Daniel Luna <danlunasc@gmail.com>
2019-11-29 12:07:28 -0300

    Adicionado lógica do endpoint /cities

--- routes/city.js
+++ routes/city.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 
+const {retrieveCityById} = require('../helpers');
+
 router.get('/cities', (req, res) => {
-    
+    return res.json(retrieveCitiesArray())
 })
 
 router.get('/cities/weather', (req, res) => {
 })
 
 router.get('/cities/:id/weather', (req, res) => {
-    
+
 })
 
 
